c0d842e95bd07cf2a37b20b05400082867025129
it21128936 <kimirajakaruna@gmail.com>
2023-05-16 17:09:12 +0530

    created AllSales.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
-import './assets/css/App.css';
-import './assets/css/icons.css';
-import './assets/css/main.css';
+import "./assets/css/App.css";
+import "./assets/css/icons.css";
+import "./assets/css/main.css";
 
-import Login from './components/Authentication/Login';
-import Register from './components/Authentication/Register';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AdminDashboard from './components/Admin/EC_Monitoring/AdminDashboard';
+import Login from "./components/Authentication/Login";
+import Register from "./components/Authentication/Register";
+import AddSales from "./components/Sales/AddSales";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminDashboard from "./components/Admin/EC_Monitoring/AdminDashboard";
+import AdminHeader from "./components/header";
+import AdminFooter from "./components/Admin/Common/AdminFooter";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-      <Route path="/dashboard" exact Component={AdminDashboard}/>
-      <Route path="/login" exact Component={Login}/>  
-      <Route path="/register" exact Component={Register}/>      
-      </Routes>  
-    </Router>
+    <div>
+      <AdminHeader />
+      <AddSales />
+      <AdminFooter/>
+    </div>
   );
 }
 
